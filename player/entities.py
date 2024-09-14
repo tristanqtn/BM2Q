b29f1064f6397102278f4840f1fdf6df419c9f30
tristanqtn <tristan.querton@gmail.com>
2024-09-14 22:46:45 +0200

    feat: add game api

--- player/entities.py
+++ player/entities.py
@@ -1,12 +1,12 @@
 import random
 import json
 
+
 # Description: This file contains the Player class which is used to represent a player in the game.
 class Player:
     def __init__(self, name):
         self.id = None
         self.name = name
-        self.score = 0
         self.cards = []
         self.selected_card = None
         self.is_winner = False
@@ -51,21 +51,22 @@ class Deck:
 class Game:
 
     def load_questions(self):
-        with open('./data/questions.json', 'r') as file:
+        with open('../data/questions.json', 'r') as file:
             questions = json.load(file)
             for question in questions:
                 self.question_deck.cards.append(QuestionCard(question["text"] , question["pick"]))
             self.question_deck.shuffle()
 
     def load_answers(self):
-        with open('./data/answers.json', 'r') as file:
+        with open('../data/answers.json', 'r') as file:
             answers = json.load(file)
             for answer in answers:
                 self.answer_deck.cards.append(AnswerCard(answer))
             self.answer_deck.shuffle()
 
     def __init__(self):
-        self.id = None
+        self.player_number = 0
+        self.players_in_game = 0
         self.players = []
         self.question_deck = Deck()
         self.answer_deck = Deck()
@@ -76,6 +77,24 @@ class Game:
 
         self.load_answers()
         self.load_questions()
+
+    def add_player(self, player):
+        self.players.append(player)
+        self.players_in_game += 1
+
+    def remove_player(self, player_id):
+        print("ID", player_id)
+        for player in self.players:
+            #print(player.id.ty, player_id.type, player.id == player_id)
+            if str(player.id) == player_id:
+                self.players.remove(player)
+        self.players_in_game -= 1
+        for player in self.players:
+            print(player.name, player.id)
+
+    def show_game_config(self):
+        print("Number of players: ", self.player_number)
+        print("Players in game: ", self.players_in_game)
 
     def show_decks(self):
         for question in self.question_deck.cards:
--- server/api.py
+++ server/api.py
@@ -1,8 +1,42 @@
 from fastapi import FastAPI
+from player.entities import Game, Player
+import uuid
 
 app = FastAPI()
 
+g = Game()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/game")
+async def get_game():
+    return {"message": "Game is running", "players in game": g.players_in_game, "player_number": g.player_number, "players": [player.name for player in g.players]}
+
+@app.put("/game")
+async def configure_game(number_of_players: int):
+    g.player_number = number_of_players
+    g.show_game_config()
+    return {"message": "Game configured"}
+
+@app.post("/game/join")
+async def join_game(player_name: str):
+    if g.players_in_game >= g.player_number and g.player_number != 0:
+        return {"message": "Game is full"}
+    else:
+        p = Player(player_name)
+        p.id = uuid.uuid4()
+        g.add_player(p)
+        for player in g.players:
+            print(player.name)
+        return {"message": "Player joined game", "player_id": p.id}
+
+
+
+
+@app.delete("/game/join")
+async def leave_game(player_id: str):
+    g.remove_player(player_id)
+    return {"message": "Player left game, ciao..."}
+
